8ac2d8d2f369aa1969ed2ced7c9b18d5d26f7596
talal1998 <thaheemtalal@gmail.com>
2021-12-05 20:17:17 +0000

    Created BubbleSort Algorithm

--- src/main/java/com/sparta/model/BubbleSort.java
+++ src/main/java/com/sparta/model/BubbleSort.java
@@ -1,4 +1,45 @@
 package com.sparta.model;
 
-public class BubbleSort {
+import java.util.ArrayList;
+
+public class BubbleSort implements Sorter {
+    public int[] bubbleSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i] > array[j]) {
+                    int temporaryVariable = array[i];
+                    array[i] = array[j];
+                    array[j] = temporaryVariable;
+                }
+            }
+        }
+        return array;
+    }
+
+    public ArrayList<Integer> bubbleSort(ArrayList<Integer> arrayList) {
+        int size = arrayList.size();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arrayList.get(i) > arrayList.get(j)) {
+                    int temp = arrayList.get(i);
+                    arrayList.set(i, arrayList.get(j));
+                    arrayList.set(j, temp);
+                }
+            }
+        } return arrayList;
+    }
+
+
+    @Override
+    public int[] sort(int[] array) {
+        return bubbleSort(array);
+    }
+
+    @Override
+    public ArrayList<Integer> sort(ArrayList<Integer> arrayList) {
+        return bubbleSort(arrayList);
+    }
 }
--- src/main/java/com/sparta/model/Sorter.java
+++ src/main/java/com/sparta/model/Sorter.java
@@ -1,4 +1,8 @@
 package com.sparta.model;
 
+import java.util.ArrayList;
+
 public interface Sorter {
+    int[] sort(int[] array);
+    ArrayList<Integer> sort(ArrayList<Integer> arrayList);
 }
