f13460fed157a4108e85a6a8171ef2de954fd504
talal1998 <thaheemtalal@gmail.com>
2021-12-06 04:10:23 +0000

    Added Exception Handling to View

--- src/main/java/com/sparta/view/View.java
+++ src/main/java/com/sparta/view/View.java
@@ -1,17 +1,48 @@
 package com.sparta.view;
-import com.sparta.model.Sorter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class View {
     private int listChoice = 0;
     private int sortChoice = 0;
+    private boolean listChoiceValid = true;
+    private boolean sortChoiceValid = true;
+    private Scanner scanner = new Scanner(System.in);
+
 
     public void userInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type 1 to generate a random Array or 2 to generate a random ArrayList: ");
-        setListChoice(scanner.nextInt());
-        System.out.println("Type 1 for BubbleSort, 2 for QuickSort or 3 for Binary Tree Sort: ");
-        setSortChoice(scanner.nextInt());
+
+        while(listChoiceValid) {
+            try {
+                System.out.println("Type 1 to generate a random Array or 2 to generate a random ArrayList: ");
+                setListChoice(scanner.nextInt());
+                if (getListChoice() > 0 && getListChoice() < 3) {
+                    listChoiceValid = false;
+                } else {
+                    System.out.println("Invalid input. Please try again.");
+                    listChoiceValid = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again.");
+                scanner.next();
+            }
+        }
+
+        while(sortChoiceValid) {
+            try {
+                System.out.println("Type 1 for BubbleSort, 2 for QuickSort or 3 for Binary Tree Sort: ");
+                setSortChoice(scanner.nextInt());
+                if (getSortChoice() > 0 && getSortChoice() < 4) {
+                    sortChoiceValid = false;
+                } else {
+                    System.out.println("Invalid input. Please try again.");
+                    sortChoiceValid = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again.");
+                scanner.next();
+            }
+        }
     }
 
     public int getListChoice() {
